a6da321f6b4983b618afffd7879430acbec95274
陳卓妍 <D1249326@o365.fcu.edu.tw>
2025-12-07 01:57:23 +0800

    Deploy backend + public

--- firebase.js
+++ firebase.js
@@ -3,14 +3,13 @@ const admin = require("firebase-admin");
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // 重要
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-
 module.exports = { db };
